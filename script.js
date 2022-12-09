fa07e02dadc615d402f0b693cf179f2d10e29888
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2022-12-09 16:00:48 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+// for every hour from 9 to 5, create a time block row
+    // while/for loop that loops starting at 9 and ends at 5
+        // for each loop generate or build html time block
+            // append time-block to container
+                // hour
+                    // a number corresponding with the 12 hour format
+                // textarea
+                    // show existing event text, if any and allow user to input event text
+                // save button
+                    // when clicked, store/reset all current hourly event text corresponding with the hour to localStorage
+            // increase hour by one
+            // check if hour is past, current or future and apply corresponding css class to time-block
+            
